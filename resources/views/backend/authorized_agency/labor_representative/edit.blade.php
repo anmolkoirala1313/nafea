7b28f2d62c5ddcf092d2fff854e3e9706c90da64
anmolko <anmol@canosoft.com.np>
2024-01-09 21:00:38 +0545

    agency updates

--- resources/views/backend/authorized_agency/labor_representative/edit.blade.php
+++ resources/views/backend/authorized_agency/labor_representative/edit.blade.php
@@ -14,9 +14,7 @@
                     <h4 class="card-title mb-0 flex-grow-1">{{ $page_title }}</h4>
                     <div class="flex-shrink-0">
 
-                        <div class="d-flex justify-content-sm-end gap-2">
-                            <a class="btn btn-outline-primary waves-effect waves-light" href="{{route($base_route.'show', $data['row']->id)}}">
-                                <i class="ri-eye-fill align-bottom me-1"></i></a>
+                        <div class="d-flex justify-content-sm-end">
                             <a class="btn btn-outline-success waves-effect waves-light" href="{{route($base_route.'index')}}">
                                 <i class="ri-menu-2-line align-bottom me-1"></i> {{ $page . ' List'}} </a>
                         </div>
